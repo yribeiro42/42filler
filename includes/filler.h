6818086b16a5c7ce324c58f885d9e31f9422441d
Yoann RIBEIRO <yribeiro@e3r2p20.42.fr>
2017-11-17 17:52:29 +0100

    teub

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: yribeiro <yribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 11:56:31 by yribeiro          #+#    #+#             */
-/*   Updated: 2017/11/09 16:32:47 by yribeiro         ###   ########.fr       */
+/*   Updated: 2017/11/17 17:50:27 by yribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@ typedef	struct	s_env
 	int			map_x;
 	int			map_y;
 	int			player;
+	char		**board;
 }				t_env;
 
 
 int		read_map(t_env *env);
 int		get_player(t_env *env, char *line);
 int		get_coord(t_env *env, char *line);
+int		get_board(t_env *env, char *line);
 
 #endif
 
--- srcs/reader.c
+++ srcs/reader.c
@@ -6,7 +6,7 @@
 /*   By: yribeiro <yribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 12:24:14 by yribeiro          #+#    #+#             */
-/*   Updated: 2017/11/17 15:03:22 by yribeiro         ###   ########.fr       */
+/*   Updated: 2017/11/17 17:38:14 by yribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,35 @@ int		read_map(t_env *env)
 		{
 			env->player = get_player(env, line);
 		}
-		//dprintf(2, YEL "%s\n" RESET, line);
-		if (ft_strstr(line, "Plateau"))
+		if (ft_strncmp(line, "Plateau", 7) == 0)
 		{
 			dprintf(2, RED "%s\n" RESET, line);
 			get_coord(env, line);
 		}
+		dprintf(2, GRN "%s\n" RESET, line);
+		get_board(env, line);
 		ft_strdel(&line);
+	}
+	return (0);
+}
+
+int		get_board(t_env *env, char *line)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	env->board = malloc(sizeof(char) * env->map_y);
+	while (i++ < env->map_y)
+	{
+		env->board[i] = malloc(sizeof(char) * env->map_x);
+		while (j < env->map_x)
+		{
+			env->board[i][j] = 'x';
+			printf("%c\n", env->board[i][j]);
+			j++;
+		}
+		printf("\n");
 	}
 	return (0);
 }
@@ -43,10 +65,10 @@ int		get_coord(t_env *env, char *line)
 	while (ft_isalpha(*lookup))
 		lookup++;
 	lookup++;
-	while (ft_isdigit(*(lookup++)))
+	while (ft_isdigit(*lookup))
 	{
 		env->map_x = env->map_x * 10 + (*lookup - '0');
-		//lookup++;
+		lookup++;
 	}
 	lookup++;
 	while (ft_isdigit(*lookup))
@@ -54,8 +76,8 @@ int		get_coord(t_env *env, char *line)
 		env->map_y = env->map_y * 10 + (*lookup - '0');
 		lookup++;
 	}
-	dprintf(2, YEL "[%d]" RESET, env->map_x);
-	dprintf(2, BLU "[%d]" RESET, env->map_y);
+	//dprintf(2, YEL "[%d]" RESET, env->map_x);
+	//dprintf(2, BLU "[%d]" RESET, env->map_y);
 	return (0);
 }
 
