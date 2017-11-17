24db0175bbb4bb6b1eecff2e2ecdda6aaa70df3a
Yoann RIBEIRO <yribeiro@e3r12p6.42.fr>
2017-11-17 15:08:41 +0100

    ok

--- srcs/reader.c
+++ srcs/reader.c
@@ -6,7 +6,7 @@
 /*   By: yribeiro <yribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 12:24:14 by yribeiro          #+#    #+#             */
-/*   Updated: 2017/11/09 16:32:48 by yribeiro         ###   ########.fr       */
+/*   Updated: 2017/11/17 15:03:22 by yribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ int		read_map(t_env *env)
 	int		ret;
 
 	//line = NULL;
-	while (get_next_line(STDIN_FILENO, &line) != 0)
+	while (get_next_line(0, &line) != 0)
 	{
 		if (!env->player && ft_strstr(line, "./yribeiro.filler"))
 		{
 			env->player = get_player(env, line);
-			dprintf(STDERR_FILENO, YEL "[%d]" RESET, env->player);
 		}
-		dprintf(2, RED "%s\n" RESET, line);
-		if (ft_strncmp(line, "Plateau", 2) == 0)
+		//dprintf(2, YEL "%s\n" RESET, line);
+		if (ft_strstr(line, "Plateau"))
 		{
-			dprintf(STDERR_FILENO, RED "%s\n" RESET, line);
+			dprintf(2, RED "%s\n" RESET, line);
 			get_coord(env, line);
 		}
 		ft_strdel(&line);
@@ -44,12 +43,19 @@ int		get_coord(t_env *env, char *line)
 	while (ft_isalpha(*lookup))
 		lookup++;
 	lookup++;
-	while (ft_isdigit(*lookup))
+	while (ft_isdigit(*(lookup++)))
 	{
 		env->map_x = env->map_x * 10 + (*lookup - '0');
+		//lookup++;
+	}
+	lookup++;
+	while (ft_isdigit(*lookup))
+	{
+		env->map_y = env->map_y * 10 + (*lookup - '0');
 		lookup++;
 	}
-	dprintf(STDERR_FILENO, YEL "[%d]" RESET, env->map_x);
+	dprintf(2, YEL "[%d]" RESET, env->map_x);
+	dprintf(2, BLU "[%d]" RESET, env->map_y);
 	return (0);
 }
 
